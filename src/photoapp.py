fc3cb223e5c6461e5a1c214051533b421db9d8da
Jaken <105382443+JakenW@users.noreply.github.com>
2022-11-27 16:23:20 -0500

    Update photoapp.py

--- src/photoapp.py
+++ src/photoapp.py
@@ -4,20 +4,23 @@ Created on Thu Nov 10 18:00:18 2022
 """
 
 from flask import Flask, redirect, url_for, render_template, request, session
+import sqlite3 as sql
+import os
 
 #Creating the flask app
 app = Flask(__name__)
 
-
 #Creating a secret key for the flask app. Required when using sessions.
 app.secret_key = "thebestsecretkeyintheworldyep100percentthebestnodoubt"
 
+#Defining the system path to the folder as a global variable
+#Change this on your system for it to function properly
+#On windows, the line will give you errors if your filepath has \ instead of /
+mainDir = "C:/Users/theja/Documents/photo app test/photo storage"
 
 #Route to the homepage of the website
 @app.route("/")
 def home():
-    if "date" in session:
-        session.pop("date", None)
     return render_template("index.html")
 
 
@@ -28,57 +31,30 @@ def search():
     if request.method == "POST":
         
         #This line extracts the date from the form
-        albumName = request.form["albname"]
-        albumDesc = request.form["albdes"]
-        queryPhotoName = request.form["queryName"]
-        amountPhotoRequest= request.form["amountRequest"]
+        albumName = request.form["albname"]     #Album name
+        albumDesc = request.form["albdes"]      #Album description
         
-        session["name"] = albumName            #This saves the session data so we can pass it to information
-        session["desc"] = albumDesc            #This saves the session data so we can pass it to information
-        session["query"] = queryPhotoName      #This saves the session data so we can pass it to information
-        session["amount"] = amountPhotoRequest #This saves the session data so we can pass it to information
+        #Add album information to the local database
+        con = sql.connect("photos.db")
+        cur = con.cursor()
+        cur.execute("insert into ALBUM(ALBNAME, ALBDESC) values (?,?)", (albumName, albumDesc))
+        con.commit()
         
-        return redirect(url_for("information"))
+        #Create the directory for the album locally
+        newDirPath = os.path.join(mainDir, albumName)
+        os.mkdir(newDirPath)
+        
+        #This goes to a placeholder page, we should create one which lets them know it was successfully created
+        return render_template("albcreatetest.html")
     
     else:
         #If the resuest is not a post, go to the original form
-        #Since users can create a new search from the results page, we want to clear session data
-        if "name" in session:
-            session.pop("name", None)
-        if "desc" in session:
-            session.pop("desc", None)
-            
         return render_template("addalbum.html")
 
 #Route to test passing album info
 @app.route("/information")
 def information():
-    #Check to confirm there is a date stored in the current session
-    if "name" in session:
-        #Assign the session date as a variable called date
-        name = session["name"]
-    
-    if "desc" in session:
-        #Assign the session date as a variable called date
-        desc = session["desc"]
-        
-    if "query" in session:
-        #Assign the session date as a variable called date
-        desc = session["query"]    
-        
-    if "amount" in session:
-        #Assign the session date as a variable called date
-        desc = session["amount"]
-        '''
-        Here would be code to create the directory locally.
-        I have not yet done this.
-        '''
-        
-        return render_template("information.html", name = name, desc = desc)
-    
-    else:
-        #If there is not date in the session and someone is trying to access this page, send them to the form.
-        return redirect(url_for("addalbum"))
+    return render_template("information.html")
 
 #This drives the program by creating the instance of the Seivom Backend and Running the Flask App
 if __name__ == "__main__":

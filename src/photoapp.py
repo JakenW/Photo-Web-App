c35735d8b03f06ae317544bef6cb0d2bf3430552
Jaken <105382443+JakenW@users.noreply.github.com>
2022-11-27 16:42:49 -0500

    Update photoapp.py

--- src/photoapp.py
+++ src/photoapp.py
@@ -26,7 +26,7 @@ def home():
 
 #Route to the search form which obtains the date from the user
 @app.route("/addalbum/", methods=["POST", "GET"])
-def search():
+def addalbum():
     #If the request method is post, we want to extract the date
     if request.method == "POST":
         
@@ -51,6 +51,45 @@ def search():
         #If the resuest is not a post, go to the original form
         return render_template("addalbum.html")
 
+    
+#Route obtain photos with information specified by the user
+@app.route("/getphotos/", methods=["POST", "GET"])
+def getphotos():
+    #If the request method is post, we want to extract the album information
+    if request.method == "POST":
+        
+        #This line extracts the date from the form
+        queryPhotoName = request.form["queryName"]              #search term
+        amountPhotoRequest= request.form["amountRequest"]       #number of photos
+        
+        '''
+        before returning the html template, there are a few things to do:
+        1. Modify the search parameters to represent the term and number 
+        specified by the user. (This can be done based off the apicase.py
+        file on github by modifying dictionary values)
+        2. Search the api based on the newly created search parameters (
+        this can also be based on the apicase.py file.)
+        3. Inside the for loop for results from the api, the photo needs
+        to be downloaded locally and then meta-data about each
+        image must be tracked and placed within the database.
+        
+        Grayson: Work on 1 and 2 in this area as it relates with the API,
+        if you need help I (Jaken) can help you with it
+        
+        Once the API is pulling images based on the user input information,
+        I (Jaken) will add in the code for part 3 which will collect 
+        the meta-data and store it on the database 
+        '''
+        
+        #redirect(url_for("albcreatetest.html"))
+        #Should render a placeholder template which says photos properly downloaded or something
+        #The text placeholder will need to be replaced with an HTML file 
+        return render_template("placeholder")
+    
+    else:
+        #If the resuest is not a post, go to the original form
+        return render_template("getphotos.html")
+    
 #Route to test passing album info
 @app.route("/information")
 def information():
